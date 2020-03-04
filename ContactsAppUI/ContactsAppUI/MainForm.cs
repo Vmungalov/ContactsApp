af30a5aa91cd61896a648308146f6c3b590b1bdc
Vmungalov <v_mungalov@mail.ru>
2020-03-04 10:26:08 +0700

    Измение файлов Program И MainForm

--- ContactsAppUI/ContactsAppUI/MainForm.cs
+++ ContactsAppUI/ContactsAppUI/MainForm.cs
@@ -7,15 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ContactsAppUI.Properties;
 
 namespace ContactsAppUI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
-                //
+           // PhoneNumber Xd = new PhoneNumber();
+           // Contact contact = new Contact();
         }
     }
 }
